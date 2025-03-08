3ec945a829205a12d638f5c670b19feb75ab1008
Guilherme <endlergui123@gmail.com>
2025-03-08 14:48:34 -0300

    aa

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
